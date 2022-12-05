deba7206e71b83c90a06292f1c126ecfe9eb5511
DevWebDJ <djawed.m.mazari@gmail.com>
2022-12-05 21:58:06 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <h1 className=' text-6xl font-mono text-fuchsia-800'>Hi mom</h1>
+    </div>
+  );
+}
+
+export default App;
